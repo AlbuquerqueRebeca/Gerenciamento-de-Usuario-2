1d5479f02f6b689857692f801e2df4b248429cf5
Rebeca <becalbuquerque_23@hotmail.com>
2024-11-09 17:40:59 -0300

    Primeiro commit

---
+++ src/main/java/com/projetousuario25/projetousuario25/Projetousuario25Application.java
@@ -0,0 +1,13 @@
+package com.projetousuario25.projetousuario25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetousuario25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projetousuario25Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetousuario25/projetousuario25/Projetousuario25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetousuario25.projetousuario25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projetousuario25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
