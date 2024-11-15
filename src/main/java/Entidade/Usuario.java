de3ebe006cf3f0d20949a44753413ae1f77ac2a6
Rebeca <becalbuquerque_23@hotmail.com>
2024-11-15 18:35:40 -0300

    Definindo a estrategia

--- src/main/java/Entidade/Usuario.java
+++ src/main/java/Entidade/Usuario.java
@@ -2,6 +2,7 @@ package Entidade;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -15,7 +16,7 @@ public class Usuario {
  private String senha;
  
  @Id
- @GeneratedValue
+ @GeneratedValue(strategy = GenerationType.IDENTITY) 
 
 
 
