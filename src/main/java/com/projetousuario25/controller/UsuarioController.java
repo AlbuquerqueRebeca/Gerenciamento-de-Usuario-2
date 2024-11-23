85cc9c0ad299e50443a30b74437111b5a73811dc
Rebeca <becalbuquerque_23@hotmail.com>
2024-11-23 19:37:07 -0300

    Adiconando a variavel registrarUsuario

--- src/main/java/com/projetousuario25/controller/UsuarioController.java
+++ src/main/java/com/projetousuario25/controller/UsuarioController.java
@@ -3,6 +3,7 @@ package com.projetousuario25.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +19,7 @@ public class UsuarioController {
 
 
    @PostMapping("/registrar-usuario")
-public ResponseEntity<String> r
+public ResponseEntity<String> registrarUsuario 
 
 
 
