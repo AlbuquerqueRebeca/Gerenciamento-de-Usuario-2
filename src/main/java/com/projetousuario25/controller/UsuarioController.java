46f60247f3ad5774231dd87f6f13ea7e311a343c
Rebeca <becalbuquerque_23@hotmail.com>
2024-11-23 19:23:48 -0300

    Injeção de dependencia de UsuarioRepository na classe usuarioRepository

--- src/main/java/com/projetousuario25/controller/UsuarioController.java
+++ src/main/java/com/projetousuario25/controller/UsuarioController.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.projetousuario25.repositorio.UsuarioRepository;
+
 @RestController  
 @RequestMapping("/api")
 public class UsuarioController {
 
 	
 	@Autowired 
-
+   private UsuarioRepository usuarioRepository; 
 }
