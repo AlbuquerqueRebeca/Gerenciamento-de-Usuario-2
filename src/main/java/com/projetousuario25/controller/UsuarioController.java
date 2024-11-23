cb7ea2cf15f4b02c009897d7e081633e921fca81
Rebeca <becalbuquerque_23@hotmail.com>
2024-11-23 19:45:48 -0300

    Criando a variavel salvarUsuario

--- src/main/java/com/projetousuario25/controller/UsuarioController.java
+++ src/main/java/com/projetousuario25/controller/UsuarioController.java
@@ -21,7 +21,7 @@ public class UsuarioController {
 
    @PostMapping("/registrar-usuario")
 public ResponseEntity<String> registrarUsuario(@RequestBody Usuario usuario){
-	   
+Usuario salvarUsuario = 	   
    }
 
 
