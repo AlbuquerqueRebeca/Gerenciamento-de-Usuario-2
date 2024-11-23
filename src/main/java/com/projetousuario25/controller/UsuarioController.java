132d562a5f6bd0339f9f25883fc144223fcc2a68
Rebeca <becalbuquerque_23@hotmail.com>
2024-11-23 19:29:46 -0300

    Adicionando metodo ResponseEntity do tipo string

--- src/main/java/com/projetousuario25/controller/UsuarioController.java
+++ src/main/java/com/projetousuario25/controller/UsuarioController.java
@@ -1,6 +1,7 @@
 package com.projetousuario25.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,7 @@ public class UsuarioController {
 
 
    @PostMapping 
-
+public ResponseEntity<String>
 
 
 
