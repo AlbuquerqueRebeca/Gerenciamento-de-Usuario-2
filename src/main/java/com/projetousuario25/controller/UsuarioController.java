a948f33c77dc38dc05128f66729b89203a56189c
Rebeca <becalbuquerque_23@hotmail.com>
2024-11-26 17:33:07 -0300

    Adicionando anotação @GetMapping na classe usuarioController

--- src/main/java/com/projetousuario25/controller/UsuarioController.java
+++ src/main/java/com/projetousuario25/controller/UsuarioController.java
@@ -1,10 +1,14 @@
 package com.projetousuario25.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projetousuario25.entidade.Usuario;
@@ -18,14 +22,16 @@ public class UsuarioController {
 	@Autowired 
    private UsuarioRepository usuarioRepository; 
 
-
+//Registrando Usuario
    @PostMapping("/registrar-usuario")
 public ResponseEntity<String> registrarUsuario(@RequestBody Usuario usuario){
 Usuario salvarUsuario = usuarioRepository.save(usuario);
 return ResponseEntity.ok("Usuario registrado com sucesso!! " + salvarUsuario.getNome()); 
    }
 
+ //Buscando Usuario por Email   
+@GetMapping
 
 
-
+}
 }
