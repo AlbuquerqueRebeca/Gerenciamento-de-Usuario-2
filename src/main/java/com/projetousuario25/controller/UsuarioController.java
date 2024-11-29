ba3e2b7e0a8ee8fb9dcd16632bae5c78e68a14e3
Rebeca <becalbuquerque_23@hotmail.com>
2024-11-29 17:12:33 -0300

    Adicionando anotação @DeleteMapping na classe usuarioController

--- src/main/java/com/projetousuario25/controller/UsuarioController.java
+++ src/main/java/com/projetousuario25/controller/UsuarioController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -54,7 +55,9 @@ public ResponseEntity<List<Usuario>> listarUsuario(){
 public ResponseEntity<Usuario> atualizandoPerfil(@RequestBody Usuario usuario){ 
 Usuario perfilEditado = usuarioRepository.save(usuario);
 return ResponseEntity.ok(perfilEditado); 
-		
 }
+
+//Deletar Perfil
+@DeleteMapping  
 
 }
--- src/main/java/com/projetousuario25/repositorio/UsuarioRepository.java
+++ src/main/java/com/projetousuario25/repositorio/UsuarioRepository.java
@@ -5,7 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.projetousuario25.entidade.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, String>{ 
-	
-	
 
-}
+	}  
