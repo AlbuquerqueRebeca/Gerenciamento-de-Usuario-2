9fa1c8728ebd2fa61b5303ef69dfed565a46e065
Rebeca <becalbuquerque_23@hotmail.com>
2024-12-18 18:59:37 -0300

    Todas as URLS daqui por diante são autenticadas

--- src/main/java/com/projetousuario25/security/UsuarioSecurityConfig.java
+++ src/main/java/com/projetousuario25/security/UsuarioSecurityConfig.java
@@ -41,7 +41,8 @@ public class UsuarioSecurityConfig{
 		                        .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
 		                        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) 
 		                        .authorizeHttpRequests(auth -> auth.requestMatchers("/auth/**").permitAll()
-		                        .requestMatchers("/api/registrar-usuario**").permitAll()		
+		                        .requestMatchers("/api/registrar-usuario**").permitAll()
+		                        .anyRequest().authenticated()); 
 	}
 	
 	
