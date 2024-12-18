c79470771db6224531e94945d187515bff204bcd
Rebeca <becalbuquerque_23@hotmail.com>
2024-12-18 18:33:48 -0300

    Recebendo o parametro HttpSecurity

--- src/main/java/com/projetousuario25/security/UsuarioSecurityConfig.java
+++ src/main/java/com/projetousuario25/security/UsuarioSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -30,7 +31,7 @@ public class UsuarioSecurityConfig{
 	}
 	
 	@Bean
-	public SecurityFilterChain filterChain
+	public SecurityFilterChain filterChain(HttpSecurity http) 
 	
 	
 	
