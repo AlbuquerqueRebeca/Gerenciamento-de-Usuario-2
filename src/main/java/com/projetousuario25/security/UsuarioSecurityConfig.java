a727df7ca3d8d8e19cd3375801de955da3c28191
Rebeca <becalbuquerque_23@hotmail.com>
2024-12-18 18:12:28 -0300

    Retornando o objeto authenticationManager para lidar com a autenticaçao dos usuarios

--- src/main/java/com/projetousuario25/security/UsuarioSecurityConfig.java
+++ src/main/java/com/projetousuario25/security/UsuarioSecurityConfig.java
@@ -19,7 +19,7 @@ public class UsuarioSecurityConfig{
 	
 	@Bean  
 	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-		
+		return authenticationConfiguration.getAuthenticationManager(); 
 	}
 	
 	
