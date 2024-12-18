853dbed7881abab0f7db00ff620da593e7d38244
Rebeca <becalbuquerque_23@hotmail.com>
2024-12-18 18:54:32 -0300

    Permitindo acesso a todas URLS que começam com auth

--- src/main/java/com/projetousuario25/security/UsuarioSecurityConfig.java
+++ src/main/java/com/projetousuario25/security/UsuarioSecurityConfig.java
@@ -40,6 +40,7 @@ public class UsuarioSecurityConfig{
 		             http.csrf(csrf -> csrf.disable()) 
 		                        .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
 		                        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) 
+		                        .authorizeHttpRequests(auth -> auth.requestMatchers("/auth/**").permitAll()
 	}
 	
 	
