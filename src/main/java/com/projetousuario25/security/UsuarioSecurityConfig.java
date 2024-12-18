19f1420559567fc447d6cf3f5d8f43e31ba119b6
Rebeca <becalbuquerque_23@hotmail.com>
2024-12-18 18:50:09 -0300

    Configurando a sessão como STATELESS

--- src/main/java/com/projetousuario25/security/UsuarioSecurityConfig.java
+++ src/main/java/com/projetousuario25/security/UsuarioSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.projetousuario25.security.jwt.AuthEntrypointJwt;
@@ -38,6 +39,7 @@ public class UsuarioSecurityConfig{
 		             http.cors(Customizer.withDefaults()); 
 		             http.csrf(csrf -> csrf.disable()) 
 		                        .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
+		                        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) 
 	}
 	
 	
