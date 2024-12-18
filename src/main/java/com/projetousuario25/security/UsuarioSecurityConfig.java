3adecbaf47bd3113f61654b664246c7df2c40e11
Rebeca <becalbuquerque_23@hotmail.com>
2024-12-18 18:57:27 -0300

    Permitindo acesso para URL /api/registrar-usuario

--- src/main/java/com/projetousuario25/security/UsuarioSecurityConfig.java
+++ src/main/java/com/projetousuario25/security/UsuarioSecurityConfig.java
@@ -41,6 +41,7 @@ public class UsuarioSecurityConfig{
 		                        .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
 		                        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) 
 		                        .authorizeHttpRequests(auth -> auth.requestMatchers("/auth/**").permitAll()
+		                        .requestMatchers("/api/registrar-usuario**").permitAll()		
 	}
 	
 	
