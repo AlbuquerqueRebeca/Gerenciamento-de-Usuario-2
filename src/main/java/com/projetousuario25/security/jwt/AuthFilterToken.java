021d13bdb3a9776b2d0913469fcb8c2695f3f929
Rebeca <becalbuquerque_23@hotmail.com>
2024-12-19 20:11:26 -0300

    Extraindo o token JWT

--- src/main/java/com/projetousuario25/security/jwt/AuthFilterToken.java
+++ src/main/java/com/projetousuario25/security/jwt/AuthFilterToken.java
@@ -24,6 +24,14 @@ public class AuthFilterToken extends OncePerRequestFilter{
 	 @Override
 		protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 				throws ServletException, IOException{ 
+		
+		    try {
+		    	  String jwt = getToken(request); 
+		    }
+		 
+		 
+		 
+		 
 		 
 	        }
 	 
