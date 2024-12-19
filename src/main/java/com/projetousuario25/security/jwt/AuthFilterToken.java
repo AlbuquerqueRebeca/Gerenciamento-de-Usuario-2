4094dffd078320948e2f4dae5f9af4a7f7b4c0a1
Rebeca <becalbuquerque_23@hotmail.com>
2024-12-19 20:01:45 -0300

    Injetando a dependencia UsuarioDetailsServiceImpl

--- src/main/java/com/projetousuario25/security/jwt/AuthFilterToken.java
+++ src/main/java/com/projetousuario25/security/jwt/AuthFilterToken.java
@@ -3,10 +3,13 @@ package com.projetousuario25.security.jwt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.projetousuario25.service.UsuarioDetailsServiceImpl;
+
 public class AuthFilterToken extends OncePerRequestFilter{ 
 	 
 	 @Autowired 
 	 private UsuarioJwtUtils usuarioJwtUtils; 
 	 
 	 @Autowired
+	 private UsuarioDetailsServiceImpl userDetailService; 
 }
