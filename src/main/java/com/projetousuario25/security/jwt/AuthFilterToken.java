110d6d669e9591764e9ed5436f610ec62feea199
Rebeca <becalbuquerque_23@hotmail.com>
2024-12-19 19:55:58 -0300

    Adicionando anotação @Autowired na classe authfiltertokenb

--- src/main/java/com/projetousuario25/security/jwt/AuthFilterToken.java
+++ src/main/java/com/projetousuario25/security/jwt/AuthFilterToken.java
@@ -1,8 +1,9 @@
 package com.projetousuario25.security.jwt;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 public class AuthFilterToken extends OncePerRequestFilter{ 
 	 
-	 
+	 @Autowired 
 }
