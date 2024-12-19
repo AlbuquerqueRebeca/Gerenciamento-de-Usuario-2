6d64c2538198209821d40f474bd64876c48d07f6
Rebeca <becalbuquerque_23@hotmail.com>
2024-12-19 19:11:05 -0300

    Implementando metodo commence pra lidar com as exeçoes de autenticaçao

--- src/main/java/com/projetousuario25/security/jwt/AuthEntrypointJwt.java
+++ src/main/java/com/projetousuario25/security/jwt/AuthEntrypointJwt.java
@@ -1,12 +1,26 @@
 package com.projetousuario25.security.jwt;
 
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 @Component 
-public class AuthEntrypointJwt implements AuthenticationEntryPoint{  
+public class AuthEntrypointJwt implements AuthenticationEntryPoint {
+	
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException authException) throws IOException, ServletException{ 
+		
+	}
+	
+	}  
 	
 	
 	
 
-}
+
