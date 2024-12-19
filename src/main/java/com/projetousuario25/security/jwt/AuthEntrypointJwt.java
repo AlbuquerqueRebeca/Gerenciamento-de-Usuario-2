fb09f36a663c82290ff80293513785cd43d756a0
Rebeca <becalbuquerque_23@hotmail.com>
2024-12-19 19:19:16 -0300

    Ajustando a resposta para mediatype aplication json value

--- src/main/java/com/projetousuario25/security/jwt/AuthEntrypointJwt.java
+++ src/main/java/com/projetousuario25/security/jwt/AuthEntrypointJwt.java
@@ -1,5 +1,7 @@
 package com.projetousuario25.security.jwt;
 
+import java.awt.PageAttributes.MediaType;
+
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -18,7 +20,7 @@ public class AuthEntrypointJwt implements AuthenticationEntryPoint {
 		
 		        
 		
-		                response.setContentType(null);
+		                response.setContentType(MediaType.APLICATION_JASON_VALUE);
 		
 	}
 	
