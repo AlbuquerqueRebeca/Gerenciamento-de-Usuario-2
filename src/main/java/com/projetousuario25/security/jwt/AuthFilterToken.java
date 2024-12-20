69b9b5073e14d1a3722ce8c531106f212fc08a5d
Rebeca <becalbuquerque_23@hotmail.com>
2024-12-20 16:41:06 -0300

    Adicionando verificaçao de token nao nulo

--- src/main/java/com/projetousuario25/security/jwt/AuthFilterToken.java
+++ src/main/java/com/projetousuario25/security/jwt/AuthFilterToken.java
@@ -23,10 +23,15 @@ public class AuthFilterToken extends OncePerRequestFilter{
 
 	 @Override
 		protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-				throws ServletException, IOException{ 
+				throws ServletException, IOException { 
 		
 		    try {
 		    	  String jwt = getToken(request); 
+		    	  if(jwt != null && jwtUtil.getUsernameToken(jwt)) {
+		    		  
+		    		  
+		    	  }
+		    	  
 		    }
 		 
 		 
