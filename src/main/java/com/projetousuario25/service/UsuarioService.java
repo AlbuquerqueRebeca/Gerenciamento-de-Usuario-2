f604a0b923cd672f7e7e9164a8958eb44546c31b
Rebeca <becalbuquerque_23@hotmail.com>
2024-12-20 17:51:53 -0300

    Injetandoa dependencia usuarioRepository

--- src/main/java/com/projetousuario25/service/UsuarioService.java
+++ src/main/java/com/projetousuario25/service/UsuarioService.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.projetousuario25.repositorio.UsuarioRepository;
+
 @Service 
 public class UsuarioService{
 	
@@ -11,6 +13,7 @@ public class UsuarioService{
 	private PasswordEncoder passwordEncoder; 
 	
 	@Autowired 
+	private UsuarioRepository usuarioRepository; 
 	
 	
 }
