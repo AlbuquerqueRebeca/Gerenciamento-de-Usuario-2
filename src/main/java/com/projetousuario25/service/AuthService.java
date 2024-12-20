f9abbfd9651614c9ea4bdbeb1bd95e2e76e59416
Rebeca <becalbuquerque_23@hotmail.com>
2024-12-20 19:10:04 -0300

    Injetando a dependencia AuthenticationManager

--- src/main/java/com/projetousuario25/service/AuthService.java
+++ src/main/java/com/projetousuario25/service/AuthService.java
@@ -1,12 +1,14 @@
 package com.projetousuario25.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService{
 	
 	@Autowired
+	private AuthenticationManager authenticationManager; 
 	
 	
 }
