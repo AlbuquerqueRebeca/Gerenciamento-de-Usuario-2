0322ebe21173d3ca8b5e84b50f143de5fe3d2aca
Rebeca <becalbuquerque_23@hotmail.com>
2024-12-21 19:38:30 -0300

    Adicionandos metodos padroes

--- src/main/java/com/projetousuario25/service/UsuarioDetailsImpl.java
+++ src/main/java/com/projetousuario25/service/UsuarioDetailsImpl.java
@@ -47,20 +47,44 @@ public class UsuarioDetailsImpl implements UserDetails{
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// TODO Auto-generated method stub
+		
 		return authorities;
 	}
 
 	@Override
 	public String getPassword() {
-		// TODO Auto-generated method stub
+		
 		return password;     
 	}
 
 	@Override
 	public String getUsername() {
-		// TODO Auto-generated method stub
+		
 		return username;    
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		
+		return true;
 	} 
 	
 	
