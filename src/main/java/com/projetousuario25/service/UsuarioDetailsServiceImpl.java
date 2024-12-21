4fcdb77cf83001de7811a4f646bebec10c2b6b2c
Rebeca <becalbuquerque_23@hotmail.com>
2024-12-21 18:19:44 -0300

    Adicionando o metodo padrao loadUserByUsername

--- src/main/java/com/projetousuario25/service/UsuarioDetailsServiceImpl.java
+++ src/main/java/com/projetousuario25/service/UsuarioDetailsServiceImpl.java
@@ -1,8 +1,22 @@
 package com.projetousuario25.service;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-public class UsuarioDetailsServiceImpl implements UserDetailsService{ 
+public class UsuarioDetailsServiceImpl implements UserDetailsService{
+
+	
+	
+	
+	
+	
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+	
+		return null;   
+	} 
 	
 	
 	
