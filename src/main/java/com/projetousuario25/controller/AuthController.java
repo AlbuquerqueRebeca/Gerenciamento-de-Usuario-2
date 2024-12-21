d89f4e2a4ee00031a2045e5cd061dc2fcde9fe34
Rebeca <becalbuquerque_23@hotmail.com>
2024-12-21 20:37:11 -0300

    Adicionando anotaçao @Autowired

--- src/main/java/com/projetousuario25/controller/AuthController.java
+++ src/main/java/com/projetousuario25/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.projetousuario25.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController{
 	
 	
-	
+	@Autowired
 	
 	
 }
