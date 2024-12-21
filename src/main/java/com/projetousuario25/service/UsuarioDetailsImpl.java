5724df97e75da679de8fd28f72c6deaea9b526a8
Rebeca <becalbuquerque_23@hotmail.com>
2024-12-21 19:23:54 -0300

    Adicionando parametro usuario getemail

--- src/main/java/com/projetousuario25/service/UsuarioDetailsImpl.java
+++ src/main/java/com/projetousuario25/service/UsuarioDetailsImpl.java
@@ -31,7 +31,8 @@ public class UsuarioDetailsImpl implements UserDetails{
 	
 	public static UsuarioDetailsImpl build(Usuario usuario) { 
 		
-		   return new UsuarioDetailsImpl(null, null, null, null, null) 
+		   return new UsuarioDetailsImpl(
+				   usuario.getEmail(), null, null, null, null)  
 		
 	}
 			
