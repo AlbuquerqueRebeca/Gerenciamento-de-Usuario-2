15ea004865187d115b7baa4fba6d70168b7c776d
Rebeca <becalbuquerque_23@hotmail.com>
2024-12-21 18:58:06 -0300

    Adicionando parametro string nome

--- src/main/java/com/projetousuario25/service/UsuarioDetailsImpl.java
+++ src/main/java/com/projetousuario25/service/UsuarioDetailsImpl.java
@@ -11,7 +11,7 @@ public class UsuarioDetailsImpl implements UserDetails{
 	private String password; 
 	
 	
-	public UsuarioDetailsImpl(String email, )
+	public UsuarioDetailsImpl(String email, String nome)
 	
 	
 	
