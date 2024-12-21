6660f3d15719aaf41a9a4b9a15be122795e4d512
Rebeca <becalbuquerque_23@hotmail.com>
2024-12-21 18:36:40 -0300

    Adicionando anotação @Autowired

--- src/main/java/com/projetousuario25/service/UsuarioDetailsServiceImpl.java
+++ src/main/java/com/projetousuario25/service/UsuarioDetailsServiceImpl.java
@@ -1,5 +1,6 @@
 package com.projetousuario25.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,8 +11,8 @@ import com.projetousuario25.entidade.Usuario;
 public class UsuarioDetailsServiceImpl implements UserDetailsService{
 
 	
-	
-	
+	  @Autowired 
+	  
 	
 	
 	
