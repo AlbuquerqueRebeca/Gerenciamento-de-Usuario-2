48dfcd8b285a9062cbde49ca7b642001fb24351e
Rebeca <becalbuquerque_23@hotmail.com>
2024-12-27 18:10:57 -0300

    Injetando a dependencia passwordEnconder

--- src/test/java/com/projetousuario25/projetousuario25/serviceTest/usuarioServiceTest.java
+++ src/test/java/com/projetousuario25/projetousuario25/serviceTest/usuarioServiceTest.java
@@ -2,6 +2,7 @@ package com.projetousuario25.projetousuario25.serviceTest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.projetousuario25.service.UsuarioService;
 
@@ -10,6 +11,9 @@ public class usuarioServiceTest {
 	
 	@Autowired
 	private UsuarioService usuarioService; 
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder; 
 	
 	
 
