d1d92ba30ca8d3494c1ab2ca85209244576e3190
Rebeca <becalbuquerque_23@hotmail.com>
2024-12-27 19:52:36 -0300

    Adicionando o metodo ispresent ao objeto usuario

--- src/test/java/com/projetousuario25/projetousuario25/serviceTest/usuarioServiceTest.java
+++ src/test/java/com/projetousuario25/projetousuario25/serviceTest/usuarioServiceTest.java
@@ -49,7 +49,7 @@ public class usuarioServiceTest {
 	void buscandoUsuario() { 
 	String email = "Rafaella-Floriano@hotmail.com";
 	Optional<Usuario> usuario = usuarioService.buscandoUsuario(email); 
-	assertTrue(false);
+	assertTrue(usuario.isPresent(), ); 
 		
 	}
 	
