d1c89b8599553003e38026d107cb0269851dfe64
Rebeca <becalbuquerque_23@hotmail.com>
2024-12-27 18:29:34 -0300

    Adicionando nome como parametro

--- src/test/java/com/projetousuario25/projetousuario25/serviceTest/usuarioServiceTest.java
+++ src/test/java/com/projetousuario25/projetousuario25/serviceTest/usuarioServiceTest.java
@@ -22,7 +22,7 @@ public class usuarioServiceTest {
 	void salvandoUsuario() { 
 	Usuario usuario = new Usuario(); 	
 	usuario.setEmail("Rafaella-Floriano@hotmail.com"); 
-	usuario.getNome() 
+	usuario.getNome("Rafaella Floriano");  
 		
 	}
 	
