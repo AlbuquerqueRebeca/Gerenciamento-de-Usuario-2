ffdc4bb7dff086352bc91cb39eda33393c3cec00
Rebeca <becalbuquerque_23@hotmail.com>
2024-12-27 18:07:33 -0300

    Adicionando anotação @springboottest na classe usuarioServiceTest

--- src/test/java/com/projetousuario25/projetousuario25/serviceTest/usuarioServiceTest.java
+++ src/test/java/com/projetousuario25/projetousuario25/serviceTest/usuarioServiceTest.java
@@ -1,10 +1,12 @@
 package com.projetousuario25.projetousuario25.serviceTest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest 
 public class usuarioServiceTest {
 	
+	@Autowired
 	
 	
 
