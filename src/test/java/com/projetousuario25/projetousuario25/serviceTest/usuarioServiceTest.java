96560d026eaaebc9e3e0d4644b8f7e4d65761411
Rebeca <becalbuquerque_23@hotmail.com>
2024-12-27 18:11:53 -0300

    Adicionando anotaçao @test

--- src/test/java/com/projetousuario25/projetousuario25/serviceTest/usuarioServiceTest.java
+++ src/test/java/com/projetousuario25/projetousuario25/serviceTest/usuarioServiceTest.java
@@ -1,5 +1,6 @@
 package com.projetousuario25.projetousuario25.serviceTest;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,6 +15,9 @@ public class usuarioServiceTest {
 	
 	@Autowired
 	private PasswordEncoder passwordEncoder; 
+	
+	
+	@Test
 	
 	
 
