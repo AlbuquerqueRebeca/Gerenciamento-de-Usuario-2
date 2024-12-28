1b84a31c0a1cbc56fe6400729667aefb762220ae
Rebeca <becalbuquerque_23@hotmail.com>
2024-12-28 18:32:00 -0300

    Chamando o metodo buscandoUsuario

--- src/test/java/com/projetousuario25/projetousuario25/serviceTest/usuarioServiceTest.java
+++ src/test/java/com/projetousuario25/projetousuario25/serviceTest/usuarioServiceTest.java
@@ -67,7 +67,7 @@ public class usuarioServiceTest {
 	
 	usuarioService.atualizandoPerfil(usuario); 
 	
-	Usuario perfilAtualizado = 
+	Usuario perfilAtualizado = usuarioService.buscandoUsuario(null)
 		
 	}
 	
