da0980814a08ff6df0ed046189def7716487c257
Rebeca <becalbuquerque_23@hotmail.com>
2024-12-28 18:22:33 -0300

    Adicionando o metodo setNome no objeto usuario

--- src/test/java/com/projetousuario25/projetousuario25/serviceTest/usuarioServiceTest.java
+++ src/test/java/com/projetousuario25/projetousuario25/serviceTest/usuarioServiceTest.java
@@ -59,6 +59,7 @@ public class usuarioServiceTest {
 	void atualizandoPerfil() { 
 	Usuario usuario = new Usuario();
 	usuario.setEmail("Rafaella-Floriano@hotmail.com"); 
+	usuario.setNome(null);
 		
 	}
 	
