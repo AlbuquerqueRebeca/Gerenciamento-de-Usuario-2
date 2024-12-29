e2f8934d96504a55d5ea6181d20d6a1de718c3ba
Rebeca <becalbuquerque_23@hotmail.com>
2024-12-29 13:29:58 -0300

    Adicionando o metodo getemail no parametro

--- src/test/java/com/projetousuario25/projetousuario25/serviceTest/deleteTest.java
+++ src/test/java/com/projetousuario25/projetousuario25/serviceTest/deleteTest.java
@@ -31,7 +31,7 @@ public class deleteTest {
 	 
 	 usuarioService.buscandoUsuario(usuario.getEmail()); 
 	 
-	 Optional<Usuario> deletarUsuario = usuarioService.buscandoUsuario(null)
+	 Optional<Usuario> deletarUsuario = usuarioService.buscandoUsuario(usuario.getEmail());
 		 
 	 }
 	
