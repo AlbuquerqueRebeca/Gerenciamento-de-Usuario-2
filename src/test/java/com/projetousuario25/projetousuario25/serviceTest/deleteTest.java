2f2484ce9136d130d5efe7b3646ed2c0e2be023b
Rebeca <becalbuquerque_23@hotmail.com>
2024-12-29 13:12:18 -0300

    Injetando a dependencia usuarioservice

--- src/test/java/com/projetousuario25/projetousuario25/serviceTest/deleteTest.java
+++ src/test/java/com/projetousuario25/projetousuario25/serviceTest/deleteTest.java
@@ -1,6 +1,6 @@
 package com.projetousuario25.projetousuario25.serviceTest;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Optional;
@@ -16,27 +16,10 @@ import com.projetousuario25.service.UsuarioService;
 public class deleteTest {
 	
 	
-	 @Autowired
-	private  UsuarioService usuarioService;
+	 @Autowired 
+	 private UsuarioService usuarioService;
 	
 	
-	@Test
-	void deletandoPerfil() {
-		System.out.println("iniciando teste'");
-		Usuario usuario = new Usuario(); 
-		 usuario.setEmail("Rafaella-Floriano@hotmail.com");
-		 usuario.setNome("Rafaella Floriano");
-		 usuario.setLogin("Rafaella");
-		 usuario.setSenha("24294"); 
-
-		 
-	   usuarioService.deletarPerfil(usuario.getEmail());
-        
-	   Optional<Usuario> deletarUsuario = usuarioService.buscandoUsuario(usuario.getEmail());
-        assertTrue(deletarUsuario.isEmpty(), "Perfil deletado com Sucesso");
-		 
-		 System.out.println("Perfil deletado com sucesso!!!");
-	   }
 	
 	
 
