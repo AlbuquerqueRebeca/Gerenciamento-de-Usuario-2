b10fb4ffa72b9bd4bd43433210d3f227312e9fda
Rebeca <becalbuquerque_23@hotmail.com>
2024-12-29 12:46:11 -0300

    Adicionando o metodo setSenha no objeto usuario

--- src/test/java/com/projetousuario25/projetousuario25/serviceTest/usuarioServiceTest.java
+++ src/test/java/com/projetousuario25/projetousuario25/serviceTest/usuarioServiceTest.java
@@ -83,6 +83,7 @@ public class usuarioServiceTest {
 	   usuario.setEmail("Rafaella-Floriano@hotmail.com"); 
 	   usuario.setNome("Rafaella Floriano");
 	   usuario.setLogin("Rafaella"); 
+	   usuario.setSenha(null);
 	   
 		   
 		   
